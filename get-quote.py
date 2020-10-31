9b6586ef73a5a06ccc97d1d8927178edc5399029
Tim Wigton <tim@murcadom.com>
2020-10-31 15:32:32 -0500

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -7,8 +7,9 @@ def primary():
   quotes = f.readlines()
   f.close()
   last = len(quotes)-1
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0,last)
+  rnd2 = random.randint(0,last)
+  print(quotes[rnd1]+" and "+ quotes[rnd2])
 
 if __name__== "__main__":
   primary()
